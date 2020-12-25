988c58adfe56a12ee72aa0f3f07eac1dba893094
Troy Dorman <troydorman@gmail.com>
2020-12-24 18:56:42 -0700

    basic database connect is successfully established

--- start.js
+++ start.js
@@ -5,4 +5,20 @@ const ascii = require('ascii-art');
 
 
 
+let con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "blahblah",
+    port: 3306
+});
+
+
+con.connect(err =>{
+
+    if (err) throw err;
+    console.log("connected!");
+    
+
+
+});
 
