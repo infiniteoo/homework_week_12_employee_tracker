b4e506c7b65ca0eaffb6d4efdcf68af1a5e280d6
Troy Dorman <troydorman@gmail.com>
2021-01-02 10:38:07 -0700

    half way there getting the remove an employee function working.  got the names extracted from the return object array and made as inquirer list prompts

--- start.js
+++ start.js
@@ -16,15 +16,15 @@ function mainMenu() {
         .then(response => {
 
             switch (response.mainMenu) {
-
+                // done
                 case "View All Employees":
                     viewAllEmployees();
                     break;
-
+                // done
                 case "View All Employees by Department":
                     viewAllEmployeesByDepartment();
                     break;
-
+                // done
                 case "View All Employees by Manager":
                     viewAllEmployeesByManager();
                     break;
@@ -44,7 +44,7 @@ function mainMenu() {
                 case "Update Employee Manager":
                     updateEmployeeManager();
                     break;
-
+                // done
                 case "View All Roles":
                     viewAllRoles();
                     break;
@@ -56,11 +56,11 @@ function mainMenu() {
                 case "Remove Role":
                     removeRole();
                     break;
-
+                // done
                 case "View All Departments":
                     viewAllDepartments();
                     break;
-
+                // done
                 case "Quit":
                     connection.end();
                     break;
@@ -69,6 +69,89 @@ function mainMenu() {
         });
 
 }
+
+
+function genericQueryHolder() {
+    // use this as a template for our queries
+
+    connection.query(`
+    
+    
+    
+    `, (err, res) => {
+
+
+        if (err) throw err;
+        console.table(res);
+        mainMenu();
+
+    });
+}
+
+function removeEmployee() {
+
+    // first we need to query the database and get a list of all the employees 
+
+    connection.query(`
+    SELECT
+    employee.first_name,
+    employee.last_name
+    FROM 
+    employee
+     
+    
+    `, (err, res) => {
+
+
+        if (err) throw err;
+
+        // then use that list of names as an inquirer prompt 
+        let employeeNameArray = [];
+
+        Object.keys(res).forEach(function (item) {
+
+            const first = res[item].first_name;
+            const last = res[item].last_name;
+            const complete = `${first} ${last}`;
+            employeeNameArray.push(complete);
+        });
+
+
+
+
+
+        inquirer
+            .prompt([{
+                name: "nameToRemove",
+                message: "Employee to Remove:",
+                type: "list",
+                choices: employeeNameArray
+
+            }
+
+            ])
+            .then(answers => {
+                console.log("congrats you made it this far!  venture further.");
+
+                mainMenu();
+
+            })
+            .catch(error => {
+                if (error.isTtyError) {
+                    // Prompt couldn't be rendered in the current environment
+                } else {
+                    // Something else when wrong
+                }
+            });
+
+
+
+
+
+    });
+
+};
+
 
 
 function viewAllEmployees() {
